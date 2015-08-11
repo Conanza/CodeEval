0872f0475366323559ba422ef15d4b32ff2d8846
Conan Tzou <conantzou@gmail.com>
2015-08-10 23:09:25 -0700

    solve odd numbers

---
+++ easy/odd_numbers.rb
@@ -0,0 +1,13 @@
+# Print the odd numbers from 1 to 99.
+#
+# INPUT SAMPLE:
+# There is no input for this program.
+#
+# OUTPUT SAMPLE:
+# Print the odd numbers from 1 to 99, one number per line.
+i = 1
+until i > 99
+  puts i
+
+  i += 2
+end
